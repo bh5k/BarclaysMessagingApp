3d370424eb830dc11674f267d567b28799af56c2
Bryan Hansen <bryankhansen@gmail.com>
2023-11-19 10:33:43 -0700

    Update MessageControllerIntegrationTests.java

--- src/test/java/com/barclays/controller/MessageControllerIntegrationTests.java
+++ src/test/java/com/barclays/controller/MessageControllerIntegrationTests.java
@@ -18,7 +18,7 @@ public class MessageControllerIntegrationTests {
     @Test
     public void testGetAllMessages() {
         List<Message> messages = messageController.getAllMessages("");
-        assertEquals(1, messages.size());
+        assertEquals(8, messages.size());
     }
 
     @Test
